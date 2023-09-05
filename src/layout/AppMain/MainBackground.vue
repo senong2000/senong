f76145f7d29d33839556f436e25333c8660d6b7f
senong <1120659490@qq.com>
2023-09-05 20:28:16 +0800

    feat: add rain fx

--- src/layout/AppMain/MainBackground.vue
+++ src/layout/AppMain/MainBackground.vue
@@ -1,19 +1,51 @@
 <script lang="ts" setup>
 
 const rain = ref()
+// @ts-ignore
+let timer: NodeJS.Timeout;
 
 onMounted(() => {
+    startRaining()
+})
+
+const initRain = () => {
     let raindrop = document.createElement('div')
-    raindrop.classList.add('text')
+
+    let left = Math.floor(Math.random() * window.innerWidth)
+    let size = Math.random() * 1.5;
+    let duration = Math.random() * 1;
+
+    raindrop.classList.add('raindrop')
 
     rain.value.appendChild(raindrop)
+    raindrop.innerHTML = randomRaindropText();
+    raindrop.style.left = left + 'px';
+    raindrop.style.fontSize = 0.5 + size + 'em';
+    raindrop.style.animationDuration = 10 + duration + 's';
 
-    
+    setTimeout(() => {
+        rain.value.removeChild(raindrop)
+    }, 10000)
+}
 
-})
+const startRaining = () => {
+    timer = setInterval(() => {
+        initRain()
+    }, 50)
+}
+
+const randomRaindropText = () => {
+    const text = '01';
+    const letter = text[Math.floor(Math.random() * text.length)];
+    return letter;
+}
+
+
+
 
 
 </script>
+
 <template>
     <div class="background">
         <div class="background-name" font-semibold>
@@ -27,15 +59,14 @@ onMounted(() => {
 <style lang="scss" scoped>
 .background {
     position: fixed;
-    width: 100vw;
-    height: 100vh;
     top: 0;
     left: 0;
     pointer-events: none;
 
     &-name {
-        width: 100%;
-        height: 100%;
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
         display: flex;
         justify-content: center;
         align-items: center;
@@ -48,7 +79,6 @@ onMounted(() => {
             user-select: none;
             transition: all .5s;
             z-index: 10;
-
         }
 
         span:hover {
@@ -57,5 +87,43 @@ onMounted(() => {
     }
 
 
+}
+</style>
+
+<style lang="scss">
+.background {
+    &-rain {
+        .raindrop {
+            position: absolute;
+            height: 1.5rem;
+            line-height: 1.5rem;
+            text-transform: uppercase;
+            color: $PrimaryColor-default;
+            text-shadow: 0 0 5px $PrimaryColor-default, 0 0 15px $PrimaryColor-default, 0 0 30px $PrimaryColor-default;
+            transform-origin: bottom;
+            animation: animate 1s linear forwards;
+            opacity: 0.3;
+            transition: all .3s;
+        }
+
+        @keyframes animate {
+            0% {
+                transform: translateY(0) scale(1);
+                opacity: 0.3;
+            }
+
+            70% {
+                transform: translateY(110vh) scale(1);
+                opacity: 0.2;
+            }
+
+            100% {
+                transform: translateY(110vh) scale(0);
+                opacity: 0.1;
+            }
+        }
+
+
+    }
 }
 </style>
