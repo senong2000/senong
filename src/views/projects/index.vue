3a48044e429a635a2d5b8105f9889566199b3793
senong <1120659490@qq.com>
2023-09-16 02:30:16 +0800

    feat: backup & import data json keep the senong website data

--- src/views/projects/index.vue
+++ src/views/projects/index.vue
@@ -30,10 +30,10 @@ const projects = computed(() => {
 </script>
 <template>
     <div class="projects">
-        <v-row no-gutters :theme="activeThemeName">
+        <v-row no-gutters >
             <v-col cols="6" v-for="item, idx in projects" :key="idx">
-                <v-card :ripple="true" class="projects-card" @click="router.push(`${route.path}/${item.route}`)" m-2 flex flex-items-center
-                    flex-justify-center>
+                <v-card :theme="activeThemeName" :ripple="true" class="projects-card" @click="router.push(`${route.path}/${item.route}`)" m-2 flex
+                    flex-items-center flex-justify-center>
                     <v-img :src="item.cover" cover aspect-ratio="16/9" class="projects-card-cover align-center" max-h-64>
                         <span class="projects-card-cover-title" uppercase flex justify-center text-8>{{ item.title
                         }}</span>
--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -1,21 +1,67 @@
 <script lang="ts" setup>
 import { useTheme } from '@/hooks/useTheme';
+import { TodoSpace } from '@/types/todolist';
 import { getTodoSpaces } from "@/utils/cache/localStorage"
 
 const { activeThemeName } = useTheme()
 
+type Json = {
+    name: string
+    date: Date
+    todoSpaceList: Array<TodoSpace>
+
+}
+
+const importData = () => {
+    const fileInput = document.createElement('input');
+    document.body.appendChild(fileInput);
+    fileInput.type = 'file';
+    fileInput.accept = '.json';
+
+    fileInput.addEventListener('change', async () => {
+        const file = fileInput.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.readAsText(file);
+            reader.addEventListener('load', () => {
+                const content = reader.result as string;
+                const data = JSON.parse(content);
+                console.log('文件已读取：', data);
+            });
+        }
+    });
+    fileInput.click();
+}
 
 const backups = () => {
-    console.log(getTodoSpaces())
+
+    const json: Json = {
+        name: 'backups',
+        date: new Date(),
+        todoSpaceList: getTodoSpaces(),
+    }
+
+    const jsonStr = JSON.stringify(json);
+    const blob = new Blob([jsonStr], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+
+    const link = document.createElement("a");
+    link.download = "senong-backups.json";
+    link.href = url;
+    document.body.appendChild(link);
+    link.click();
 }
 
 </script>
 <template>
     <div class="settings">
-        <v-row no-gutters :theme="activeThemeName">
+        <v-row no-gutters>
             <v-col>
-                <v-btn @click="backups">
-                    备份数据信息
+                <v-btn @click="importData" :theme="activeThemeName">
+                    导入数据
+                </v-btn>
+                <v-btn @click="backups" :theme="activeThemeName">
+                    备份数据
                 </v-btn>
             </v-col>
         </v-row>
